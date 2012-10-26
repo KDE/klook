c94b83b038270ec9e2671e75d05a03111be497de
Sergey Borovkov <sergey.borovkov@osinit.ru>
2012-10-26 12:12:34 +0400

    Fix rpm creation script

--- create_rpm.sh
+++ create_rpm.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 NAME=klook
-VER=1.0
+VER=1.1
 DIR_NAME=$NAME-$VER
 
 mkdir $DIR_NAME
 cp -rf src locale icons README CMakeLists.txt $DIR_NAME
 tar -czf $DIR_NAME.tar.gz $DIR_NAME
 rm -rf $DIR_NAME
-cp -f $DIR_NAME.tar.gz ~/rpmbuild/SOURCES
+#cp -f $DIR_NAME.tar.gz ~/rpmbuild/SOURCES
 rpm -ba klook.spec
