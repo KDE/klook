5e6580268d792ef6f112969175cd25fefbdc16c8
Nicolas Lécureuil <neoclust.kde@free.fr>
2012-11-02 00:27:35 +0100

    Make sure it creates rpms on mageia too

--- create_rpm.sh
+++ create_rpm.sh
@@ -9,4 +9,4 @@ cp -rf src locale icons README CMakeLists.txt $DIR_NAME
 tar -czf $DIR_NAME.tar.gz $DIR_NAME
 rm -rf $DIR_NAME
 #cp -f $DIR_NAME.tar.gz ~/rpmbuild/SOURCES
-rpm -ba klook.spec
+rpmbuild -ba klook.spec
